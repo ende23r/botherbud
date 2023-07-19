d5472fc544ea1b365dd924c6c03f4ab78c006882
Eric <ericandow@gmail.com>
2023-07-19 01:00:59 -0400

    Add mui. Mess with the layout and pick where to put the text history

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// import Grid from '@mui/material/Grid'; // Grid version 1
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+import Paper from '@mui/material/Paper';
+
 function App() {
   const [count, setCount] = useState(0);
 
@@ -17,29 +21,48 @@ function App() {
   }, []);  // Run once on mount
 
   return (
-    <>
-      <div>
+    <Grid container spacing={1}>
+      <Grid xs={8}>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
+      </Grid>
+      <Grid xs={4}><h1>Vite + React</h1></Grid>
+      <Grid xs={4}>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+      </Grid>
+      <Grid xs={8} className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </Grid>
+      {/*temporarily setting this to 12 wide to get it on its own line*/}
+      <Grid container xs={12}>
+        <ChatLog />
+      </Grid>
+    </Grid>
   )
+}
+
+function ChatLog(props) {
+  const {messages} = props;
+  console.log({messages});
+  return (
+    <>
+    <Grid xs={8}>
+    <Paper>This is where my text will go!</Paper>
+    </Grid>
+    <Grid xs={4}><Paper>This can be a sidebar</Paper></Grid>
+    </>
+  );
 }
 
 export default App

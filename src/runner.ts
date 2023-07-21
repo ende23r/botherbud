87ff18bb58bcd8be05a4b752045ab1380be7d588
Eric <ericandow@gmail.com>
2023-07-21 00:45:08 -0400

    First successful test! runner.ts now contains valid code for creating a new listener and publishing a message to an existing topic

--- src/runner.ts
+++ src/runner.ts
@@ -8,28 +8,30 @@ type ProjectConfig = {
 };
 */
 
+const pubSub = new PubSub({ projectId: "steel-cursor-393404" });
+
 async function pubsubtest() {
-  const pubSub = new PubSub({ projectId: "steel-cursor-393404" });
-
-  const [topic] = await pubSub.createTopic("fakeirc");
-  console.log(`Topic ${topic.name} created`);
-
-  const [subscription] = await topic.createSubscription("its me, hi, I'm the problem");
+  const [subscription] = await pubSub.createSubscription("fakeirc", "listener");
+  console.log(`Subscription ${subscription.name} created`);
 
   // Receive callbacks for new messages on the subscription
   subscription.on('message', (message: any) => {
     console.log('Received message:', message.data.toString());
-    process.exit(0);
+
+    message.ack();
   });
 
   // Receive callbacks for errors on the subscription
   subscription.on('error', (error: any) => {
     console.error('Received error:', error);
-    process.exit(1);
+
+    error.ack();
   });
 
+  const topic = pubSub.topic("fakeirc");
   // Send a message to the topic
-  // topic.publishMessage({data: Buffer.from("You'll never guess what just happened")});
+  const messageID = await topic.publishMessage({data: Buffer.from("You'll never guess what just happened")});
+  console.log(`Sent message ${messageID}`);
 }
 
 pubsubtest();

0e65198521a2267c6a00fca61baae93bcaf92589
Eric <ericandow@gmail.com>
2023-07-23 18:20:50 -0400

    Fix up `npm run dev` command in server to compile and run incrementally (does not support adding files).
    Also try to .gitignore build directories.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello, world!");
+import { func } from "./lib";
+import * as express from "express";
+
+const PORT = 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("Processing request");
+  res.send("Success!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Botherbud server listening on port ${PORT}`);
+});
+
